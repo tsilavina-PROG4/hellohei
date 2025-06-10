391e4921982fe87da520be125f19a82bb0e1f525
tsilavina007 <andriamiart114@gmail.com>
2025-06-10 16:20:18 +0300

    ✨ feat: file upload to S3 bucket

--- src/main/java/school/prog/hellohei/endpoint/rest/controller/hello/HelloController.java
+++ src/main/java/school/prog/hellohei/endpoint/rest/controller/hello/HelloController.java
@@ -4,20 +4,23 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.prog.hellohei.PojaGenerated;
+import school.prog.hellohei.endpoint.rest.service.hello.HelloWorldService;
 
 @PojaGenerated
 @RestController
 @AllArgsConstructor
 public class HelloController {
 
+  private final HelloWorldService service;
   public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
   public static final ResponseEntity<String> KO =
       new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
 
   @GetMapping("/hello")
-  public String hello() {
-    return "... world!";
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
   }
 }
