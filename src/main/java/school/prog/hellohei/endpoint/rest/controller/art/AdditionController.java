e2cea493078219710a39ca782ee87e7d9549c7dc
tsilavina007 <andriamiart114@gmail.com>
2025-07-15 12:18:51 +0300

    feat: sentry test

--- src/main/java/school/prog/hellohei/endpoint/rest/controller/art/AdditionController.java
+++ src/main/java/school/prog/hellohei/endpoint/rest/controller/art/AdditionController.java
@@ -1,5 +1,6 @@
 package school.prog.hellohei.endpoint.rest.controller.art;
 
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdditionController {
   @GetMapping("/add")
   public long addition(@RequestParam long a, @RequestParam long b) {
+
+    try {
+      throw new Exception("This is a test.");
+    } catch (Exception e) {
+      Sentry.captureException(e);
+    }
     if (a < 0 || b < 0) {
       log.warn("Negative inputs are not allowed");
     } else if (a + b < 0) {
