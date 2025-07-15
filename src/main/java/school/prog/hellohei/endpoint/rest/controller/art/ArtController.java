2de022c63342ec143c2ec4d07defe018299c09ed
tsilavina007 <andriamiart114@gmail.com>
2025-07-15 11:01:55 +0300

    refactor: arith

--- src/main/java/school/prog/hellohei/endpoint/rest/controller/art/ArtController.java
+++ src/main/java/school/prog/hellohei/endpoint/rest/controller/art/ArtController.java
@@ -1,26 +1,20 @@
 package school.prog.hellohei.endpoint.rest.controller.art;
 
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import school.prog.hellohei.PojaGenerated;
-import school.prog.hellohei.service.art.ArtService;
 
-@PojaGenerated
+@Slf4j
 @RestController
-@AllArgsConstructor
-public class ArtController {
-
-  private final ArtService service;
-  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
-  public static final ResponseEntity<String> KO =
-      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
-
+public class AdditionController {
   @GetMapping("/add")
-  public Long helloWorld(@RequestParam Long a, @RequestParam Long b) {
-    return service.add(a, b);
+  public long addition(@RequestParam long a, @RequestParam long b) {
+    if (a < 0 || b < 0) {
+      log.warn("Negative inputs are not allowed");
+    } else if (a + b < 0) {
+      log.error("Inputs are too large");
+    }
+    return a + b;
   }
 }
